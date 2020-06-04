7574e5cc5a57657417492051a868948590d4a9bf
LORJIM <jdsgomezleon@gmail.com>
2020-06-04 20:02:42 +0200

    Varios Componentes Añadidos
    Fecha actual, botón de cerrar sesión, panel de navegación y pie de
    página.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { NavegacionComponent } from './navegacion/navegacion.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    NavegacionComponent
+    NavegacionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     FontAwesomeModule,
     BrowserAnimationsModule
--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-cabecera',
   templateUrl: './cabecera.component.html',
@@ -7,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabeceraComponent implements OnInit {
 
+	fecha:string;
+	valorBusqueda:string;
   constructor() { }
 
   ngOnInit(): void {
+	var d = new Date();
+	var anio=d.getFullYear();
+	const mes = d.toLocaleString('default', { month: 'short' });
+	var dia=d.getDate();
+	this.fecha=dia+" "+mes+" "+anio;
   }
 
 }
