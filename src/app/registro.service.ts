63960f664f3ee743963877b542dfb3e711532013
LORJIM <jdsgomezleon@gmail.com>
2020-06-07 12:46:04 +0200

    Registro finalizado
    Encriptación de contraseñas y comprobación de la existencia de usuarios
    y emails ya registrados.

--- src/app/registro.service.ts
+++ src/app/registro.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import {Registro} from './registro';
 import {Observable} from 'rxjs';
 
@@ -12,12 +13,18 @@ export class RegistroService {
 	private _url:string="http://127.0.0.1/ecopocket/registro.php";
 	private mensajeSource = new BehaviorSubject(null);
   	mensajeActual = this.mensajeSource.asObservable();
-  constructor(private router: Router,private http:HttpClient) { }
+	mensaje:string;
+  constructor(private router: Router,private http:HttpClient,private snackBar: MatSnackBar) { }
 
 	Registro(datos){
 		return this.http.post(this._url,datos).subscribe(data=>{
 			this.mensajeSource.next(data);
-			this.router.navigate(['/login']); //navega al login
+			this.mensajeActual.subscribe(mensaje => this.mensaje = mensaje);
+			if(this.mensaje.substring(0,1)!='S'){ //si el mensaje de vuelta no es Se ha registrado correctamente, significa que el registro no se ha realizado
+				let snackBarRef = this.snackBar.open(this.mensaje,"",{duration: 3000});
+			}else{ //si el registro se ha realizado bien, navega al login
+				this.router.navigate(['/login']);
+			}
 		});  
 	}
 }
