ee807a9e46df52ad91bc4ddab8b05a0c637ec28f
LORJIM <jdsgomezleon@gmail.com>
2020-07-04 20:16:26 +0200

    Alta de Operación Fondos de Inversión II
    En el anterior commit no subí la parte back, aquí está.

--- src/assets/server/ecopocket/fondos/insertar.php
+++ src/assets/server/ecopocket/fondos/insertar.php
@@ -18,14 +18,26 @@
 		$porcdividendo=$obj['porcdividendo'];
 		$detalles=$obj['detalles'];
 		$fecha=$obj['fecha'];
-
+		
+		
 		require "../conexion.php";
 		if($conexion->connect_errno){
 			exit("Conexion fallida. Razon: ".$conexion->connect_error);
 		}else{
-			$insertar = "INSERT INTO Fondos( Usuario, Fecha, Tipo, Cantidad, Empresa, Rentabilidad, Dividendo, PorcentajeDiv, Detalles) VALUES ('$user', '$fecha', '$tipo' ,'$cantidad', '$empresa', '$rentabilidad', '$dividendo', '$porcdividendo', '$detalles')";
+			if($porcdividendo==null){ //si no viene informado el porcdividendo, en la query su variable no puede ser un null entre comillas, tiene que insertarse sin ellas
+				$insertar = "INSERT INTO Fondos( Usuario, Fecha, Tipo, Cantidad, Empresa, Rentabilidad, Dividendo, PorcentajeDiv, Detalles) VALUES ('$user', '$fecha', '$tipo' ,'$cantidad', '$empresa', '$rentabilidad', '$dividendo', NULL, '$detalles')";
+			}else{
+				$insertar = "INSERT INTO Fondos( Usuario, Fecha, Tipo, Cantidad, Empresa, Rentabilidad, Dividendo, PorcentajeDiv, Detalles) VALUES ('$user', '$fecha', '$tipo' ,'$cantidad', '$empresa', '$rentabilidad', '$dividendo', '$porcdividendo', '$detalles')";
+			}
+			
 			if($conexion->query($insertar)){
-				$mensaje="Nuevo movimiento insertado con éxito";
+				$seleccionar = "SELECT * FROM Profitfondos WHERE Usuario ='".$user."' AND Fecha='".$fecha."'";
+				$resultados = $conexion->query($seleccionar);//basandonos en el usuario y la fecha, comprobamos si ya existe un profit del dia asociado
+
+				if($resultados -> num_rows == 0){ //si no hay resultados, significa que no hay profit del dia asociado y esta es la primera operacion, por lo que inicializamos el profit a cero, esto es necesario hacerlo para que la consulta previa de operaciones no falle
+					$conexion->query("INSERT INTO Profitfondos( Fecha, Usuario, Profit) VALUES ('$fecha', '$user', '0')");
+				}
+				$mensaje="Nueva operación insertada con éxito";
 			}
 			else{
 				$mensaje="Ha ocurrido un error inesperado";
